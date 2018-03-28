cb4df94e399b37f216cea5974e37c2596fbe2cf1
Tony Fruzza <anthony.fruzza@sturdynetworks.com>
2018-03-28 10:15:50 -0700

    inc

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'tony240zt@gmail.com'
 license          ''
 description      'AWS CloudWatch custom metrics made easy'
 long_description 'Use for generating AWS CloudWatch metrics such as disk space, counters, and more'
-version          '0.1.0'
+version          '0.1.1'
 
 requires 'aws', '~> 7.3.1'
