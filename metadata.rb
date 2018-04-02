7db6763a2d42739248baba9c2a6d8fed411cc3ac
Tony Fruzza <anthony.fruzza@sturdynetworks.com>
2018-04-02 12:07:30 -0700

    kinda a bit thing

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'support@onica.com'
 license          'All Rights Reserved'
 description      'AWS CloudWatch custom metrics made easy'
 long_description 'Use for generating AWS CloudWatch metrics such as disk space, counters, and more'
-version          '0.1.9'
+version          '0.2.0'
 
 requires 'aws', '~> 7.3.1'
